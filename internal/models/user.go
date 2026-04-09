28874420efb6bcd6ccf86f61bdccd1e4311dc297
MaxDrattcev <adratcev@mail.ru>
2026-04-09 13:21:08 +0300

    added teg JSON in model user

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type User struct {
-	ID             int64     `db:"id"`
-	Login          string    `db:"login"`
-	Password       string    `db:"password"`
-	CurrentBalance int64     `db:"current_balance"`
-	TotalWithdrawn int64     `db:"total_withdrawn"`
-	CreatedAt      time.Time `db:"created_at"`
+	ID             int64     `db:"id" json:"-"`
+	Login          string    `db:"login" json:"login"`
+	Password       string    `db:"password" json:"password"`
+	CurrentBalance int64     `db:"current_balance" json:"-"`
+	TotalWithdrawn int64     `db:"total_withdrawn" json:"-"`
+	CreatedAt      time.Time `db:"created_at" json:"-"`
 }
 
 type BalanceResponse struct {
